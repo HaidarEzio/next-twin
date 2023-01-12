4f9ebd1a12be917e48fc279800c2ff251bf2c02e
Haidar Ezio <haidar.ezio2014@gmail.com>
2023-01-12 22:58:38 +0100

    Twin.macro init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// pages/_app.tsx
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import GlobalStyles from "../styles/GlobalStyles";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
